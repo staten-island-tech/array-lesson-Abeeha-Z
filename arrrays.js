90c691fbd726a47277dc394d977ce52905e73572
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-07 13:57:55 -0400

    hghj

---
+++ arrrays.js
@@ -0,0 +1,11 @@
+//log each students name and grade
+const students = [
+    { name: 'Alice', age: 20, grade: 85 },
+    { name: 'Bob', age: 22, grade: 90 },
+    { name: 'Charlie', age: 21, grade: 78 },
+    { name: 'Diana', age: 23, grade: 95 },
+  ];
+
+// students.forEach((student) => console.log(student.name | student.grade));
+
+const youngStudents = students.filter((student) => (student.age < 21));
